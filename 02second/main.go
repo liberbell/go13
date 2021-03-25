9d39faf243a3decf6a422086be1a1c3fd2bc7d1a
Liberbell <liberbell@gmail.com>
2021-03-25 09:17:43 +0900

    main3

--- 02second/main.go
+++ 02second/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-const ok = true
+// const ok = true
+
+func nope() {
+	const ok = true
+	var hello = "HELLO"
+}
 
 func main() {
 	// fmt.Println(("Hello World"))
-	var hello = "Hello"
+	// var hello = "Hello"
 	fmt.Println(hello, ok)
 }
