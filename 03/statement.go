08af934fc5b47805899fbecb4b12ee1ea53c541d
Liberbell <liberbell@gmail.com>
2021-03-26 09:02:27 +0900

    statement5

--- 03/statement.go
+++ 03/statement.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("Hello")
 	fmt.Println(" World" + "!")
-	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.NumCPU() + 1)
 
 	if 5 > 1 {
 		fmt.Println("Bigger")
