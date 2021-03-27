b2b2b5f09270d9be4212e05619e931f5c0f4d19d
Liberbell <liberbell@gmail.com>
2021-03-27 12:33:56 +0900

    main2

--- 04/printer/cmd/main.go
+++ 04/printer/cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"/Users/hideakiehara/virtualenv/go13/04/printer/printer"
+
+	"github.com/inancgumus/learngo/first/printer"
+)
+
 func main() {
-	printer.hello()
+	printer.Hello()
 }
