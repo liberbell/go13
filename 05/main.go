3d4807e778e89de506999155df448ac5624680f4
Liberbell <liberbell@gmail.com>
2021-03-28 14:06:15 +0900

    main3

--- 05/main.go
+++ 05/main.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-var speed int
+// var speed int
 
 func main() {
 	// fmt.Println(-200, -100, 0, 50, 100, 200)
 	// fmt.Println(-50.1, -20.4, -0, 1., 100.26)
 	// fmt.Println(true, false)
 	// fmt.Println("", "hi", "こんにちは")
+	var speed int
 
-	fmt.Println(("hello"))
+	fmt.Println(speed)
 }
