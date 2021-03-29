84812dc40507fb7fe9c4b9df8b747b7fc6214b38
Liberbell <liberbell@gmail.com>
2021-03-29 11:49:16 +0900

    main2

--- 05/main.go
+++ 05/main.go
@@ -39,8 +39,10 @@ func main() {
 	var yourAge int
 	var temperature float64
 	var success bool
+	var language string
 
 	fmt.Println(myAge, yourAge)
 	fmt.Println(temperature)
-	fmt.Println((success))
+	fmt.Println(success)
+	fmt.Println(language)
 }
