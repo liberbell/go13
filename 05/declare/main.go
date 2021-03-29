7587cd4eedc48154fe548d3a287c31c3aed1d040
Liberbell <liberbell@gmail.com>
2021-03-29 12:00:12 +0900

    main4

--- 05/declare/main.go
+++ 05/declare/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	safe := true
+	// safe := true
 
-	fmt.Println(safe)
+	// fmt.Println(safe)
+
+	var name = "Carl"
+	var isScientist = true
+	var age = 62
+	var degree float64 = 5.
+
+	fmt.Println(name, isScientist, age, degree)
 }
