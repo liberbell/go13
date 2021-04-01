85963c321c273314a40da767cf2c6fcbe0a91a5a
Liberbell <liberbell@gmail.com>
2021-04-01 12:41:42 +0900

    main8

--- 05/conversion/main.go
+++ 05/conversion/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	speed := 100
-	force := 2.5
+	// speed := 100
+	// force := 2.5
 
-	// speed = speed * int(force)
-	speed = int(float64(speed) * force)
-	fmt.Println(speed)
+	// // speed = speed * int(force)
+	// speed = int(float64(speed) * force)
+	// fmt.Println(speed)
 
 	var apple int
 	var orange int32
