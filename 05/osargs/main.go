303622cb5e8ae8c30264ee6b0e542ec40bd0f084
Liberbell <liberbell@gmail.com>
2021-04-02 09:10:44 +0900

    main2

--- 05/osargs/main.go
+++ 05/osargs/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var name string
 
 	name = os.Args[1]
+	name, age := "gandalf", 2019
+
+	fmt.Println("Hello great", name, "!")
 }
