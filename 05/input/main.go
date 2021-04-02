32dde91d58e222524fa0ee13092d6d3229ee7455
Liberbell <liberbell@gmail.com>
2021-04-02 09:06:05 +0900

    main6

--- 05/input/main.go
+++ 05/input/main.go
@@ -10,4 +10,7 @@ func main() {
 	fmt.Println("Path:", os.Args[0])
 	fmt.Println("1st argument: ", os.Args[1])
 	fmt.Println("2nd argument: ", os.Args[2])
+	fmt.Println("3rd argument: ", os.Args[3])
+
+	fmt.Println("Number of items: ", len(os.Args))
 }
