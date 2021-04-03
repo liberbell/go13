d5e232f401c5614f5796447638277cb247f28126
Liberbell <liberbell@gmail.com>
2021-04-03 13:17:06 +0900

    main2

--- 05/printfunc/main.go
+++ 05/printfunc/main.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	var brand = "Google"
-	fmt.Printf("%s\n", brand)
+	// var brand = "Google"
+	// fmt.Printf("%s\n", brand)
 
-	ops := 250
-	ok := 543
-	fail := 433
+	// ops := 250
+	// ok := 543
+	// fail := 433
 
-	fmt.Printf("total: %d success: %d / %d\n", ops, ok, fail)
+	// fmt.Printf("total: %d success: %d / %d\n", ops, ok, fail)
+	var speed int
+	var heat flo6
 }
