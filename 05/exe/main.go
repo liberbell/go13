bd70a4a3ef584ae4dc70fa7538cf91527b379daf
Liberbell <liberbell@gmail.com>
2021-04-04 20:35:28 +0900

    main6

--- 05/exe/main.go
+++ 05/exe/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		myAge   = 30
-		yourAge = 35
-		average float64
-	)
-	average = float64(myAge+yourAge) / 2
-	fmt.Println(average)
+	// var (
+	// 	myAge   = 30
+	// 	yourAge = 35
+	// 	average float64
+	// )
+	// average = float64(myAge+yourAge) / 2
+	// fmt.Println(average)
 
-	ratio := 1.0 / 10
-	fmt.Printf("%.60f", ratio)
+	// ratio := 1.0 / 10
+	// fmt.Printf("%.60f", ratio)
+	fmt.Println(("Sum :", 3+2))
 }
