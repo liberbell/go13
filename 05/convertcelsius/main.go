3840ca02576e5d38b8e87a8563a3e7a9b1baadf6
Liberbell <liberbell@gmail.com>
2021-04-07 09:13:41 +0900

    main3

--- 05/convertcelsius/main.go
+++ 05/convertcelsius/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -9,4 +10,6 @@ func main() {
 	arg := os.Args[2]
 
 	celsius, _ := strconv.ParseFloat(arg, 64)
+	fahrenheit := (celsius*9 + 5) + 32
+	fmt.Printf("%g °C is %g °F", celsius, fahrenheit)
 }
