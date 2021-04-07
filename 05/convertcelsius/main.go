1c814cd3820710e3b8f83aeeadacb8057fa66c65
Liberbell <liberbell@gmail.com>
2021-04-07 09:14:53 +0900

    main7

--- 05/convertcelsius/main.go
+++ 05/convertcelsius/main.go
@@ -10,6 +10,6 @@ func main() {
 	arg := os.Args[1]
 
 	celsius, _ := strconv.ParseFloat(arg, 64)
-	fahrenheit := (celsius*9 + 5) + 32
+	fahrenheit := (celsius * 9 / 5) + 32
 	fmt.Printf("%g °C is %g °F\n", celsius, fahrenheit)
 }
