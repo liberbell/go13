9554ea70973f8e95479ccc977508b3c8792f2c2c
Liberbell <liberbell@gmail.com>
2021-04-09 08:58:37 +0900

    main4

--- 05/strlength/main.go
+++ 05/strlength/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"unicode/utf8"
 )
 
 func main() {
 	name := "漢字"
 	fmt.Println(utf8.RuneCountInString(name))
+
+	msg := os.Args[1]
+	l := len(msg)
+	s := msg + strings.Repeat("!", l)
+	fmt.Println(s)
 }
