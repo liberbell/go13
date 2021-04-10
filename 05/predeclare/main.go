64c5732d56ee43c7a21ec10102e46da9c7e00304
Liberbell <liberbell@gmail.com>
2021-04-10 10:14:09 +0900

    main10

--- 05/predeclare/main.go
+++ 05/predeclare/main.go
@@ -21,4 +21,8 @@ func main() {
 	var n int8 = math.MaxInt8
 	fmt.Println("max int8    :", n)
 	fmt.Println("max int8 + 1:", n+1)
+
+	n = math.MinInt8
+	fmt.Println("min int8    :", n)
+	fmt.Println("min int8 -1 :", n-1)
 }
