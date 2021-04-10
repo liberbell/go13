6e100a6f54a66bf49fe1684bff555489f2dd377b
Liberbell <liberbell@gmail.com>
2021-04-10 10:09:07 +0900

    main7

--- 05/predeclare/main.go
+++ 05/predeclare/main.go
@@ -1,17 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
 func main() {
-	var (
-		width  uint8 = 255
-		height       = 255
-	)
+	// var (
+	// 	width  uint8 = 255
+	// 	height       = 255
+	// )
 
-	width++
-	if int(width) < height {
-		// fmt.Println(width)
-		fmt.Println("height is greater.")
-	}
-	fmt.Printf("width: %d, height: %d\n", width, height)
+	// width++
+	// if int(width) < height {
+	// 	// fmt.Println(width)
+	// 	fmt.Println("height is greater.")
+	// }
+	// fmt.Printf("width: %d, height: %d\n", width, height)
+	int8(math.MaxInt8 + 1)
 }
