a42f43074b70e1acb245fd95b3cf5ec5bf852a86
Liberbell <liberbell@gmail.com>
2021-04-11 12:59:52 +0900

    main5

--- 05/types/main.go
+++ 05/types/main.go
@@ -10,9 +10,9 @@ func main() {
 	)
 
 	var (
-		salt Gram = 100
-		apple = Kilogram = 5
-		truck Ton = 10
+		salt  Gram     = 100
+		apple Kilogram = 5
+		truck Ton      = 10
 	)
 
 	fmt.Printf("salt: %d, apple: %d, truck: %d\n", salt, apple, truck)
