5598e3ac484d6d6789c621147c97f273b47d0fa8
Liberbell <liberbell@gmail.com>
2021-04-11 13:09:00 +0900

    weight4

--- 05/types/weight/weight.go
+++ 05/types/weight/weight.go
@@ -6,10 +6,4 @@ type (
 	Ton      Kilogram
 )
 
-var (
-	salt  Gram     = 100
-	apple Kilogram = 5
-	truck Ton      = 10
-)
-
-fmt.Printf("salt: %d, apple: %d, truck: %d", salt, apple, truck)
+// fmt.Printf("salt: %d, apple: %d, truck: %d", salt, apple, truck)
