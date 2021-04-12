06838d23b4572baeb5bf8232c5944aaf99a1e900
Liberbell <liberbell@gmail.com>
2021-04-13 08:42:21 +0900

    main2

--- 05/constant/main.go
+++ 05/constant/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	const meters 
 	cm := 100
-	m := cm / 100
+	m := cm / meters
 
 	fmt.Printf("%dcm is %dm\n", cm, m)
 
 	cm = 200
-	m = cm / 100
+	m = cm / meters
 	fmt.Printf("%dcm is %dm.\n", cm, m)
 }
