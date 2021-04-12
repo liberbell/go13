1b9a0017324b234f439f58285331e63f88ea4cac
Liberbell <liberbell@gmail.com>
2021-04-13 08:45:28 +0900

    main4

--- 05/constant/main.go
+++ 05/constant/main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	const meters int = 100
-	cm := 100
-	m := cm / meters
+	// const meters int = 100
+	// m := cm / meters
 
-	fmt.Printf("%dcm is %dm\n", cm, m)
+	// fmt.Printf("%dcm is %dm\n", cm, m)
 
-	cm = 200
-	m = cm / meters
-	fmt.Printf("%dcm is %dm.\n", cm, m)
+	// cm = 200
+	// m = cm / meters
+	// fmt.Printf("%dcm is %dm.\n", cm, m)
+	total, numberOf := 5, 0
+	fmt.Printf(total / numberOf)
 }
