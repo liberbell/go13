e0988934026f3ad9638165f73dc86a81d04de7d5
Liberbell <liberbell@gmail.com>
2021-04-13 09:01:26 +0900

    main8

--- 05/constantrule/main.go
+++ 05/constantrule/main.go
@@ -6,4 +6,11 @@ func main() {
 	// n := 2
 	const max int = len("hello")
 	fmt.Println(max)
+
+	const min int = 1
+	const pi float64 = 3.14
+	const version string = "2.0.1"
+	const debug bool = false
+
+	fmt.Println(min, pi, version, debug)
 }
