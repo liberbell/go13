30fa81127beef27eae7324159f6b05157abc80b2
Liberbell <liberbell@gmail.com>
2021-04-14 08:43:26 +0900

    main3

--- 05/typeless/main.go
+++ 05/typeless/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	const min = 1 + 1
-	const pi = 3.14 * min
+	// const min = 1 + 1
+	// const pi = 3.14 * min
 
-	fmt.Printf("min is %d, pi is %g\n", min, pi)
+	// fmt.Printf("min is %d, pi is %g\n", min, pi)
+
+	const min int = 42
+	var i int
+	fmt.Println(i)
 }
