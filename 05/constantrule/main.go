c8a60f2fc9f0955c3e9f5c31cf0eab7f51b8802e
Liberbell <liberbell@gmail.com>
2021-04-13 09:11:00 +0900

    main14

--- 05/constantrule/main.go
+++ 05/constantrule/main.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 	// n := 2
-	const max int = len("hello")
-	fmt.Println(max)
+	// const max int = len("hello")
+	// fmt.Println(max)
 
-	const min = 1 + 1
-	const pi = 3.14 * min
-	const version = "2.0.1" + "-beta"
-	const debug = !false
+	// const min = 1 + 1
+	// const pi = 3.14 * min
+	// const version = "2.0.1" + "-beta"
+	// const debug = !false
 
-	fmt.Println(min, pi, version, debug)
+	// fmt.Println(min, pi, version, debug)
+	const min, max int = 1, 1000
+	fmt.Println(max, min)
 }
