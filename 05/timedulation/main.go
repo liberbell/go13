15d6190750a9dddb67d338a528ba9d2f0a7f0db6
Liberbell <liberbell@gmail.com>
2021-04-15 08:53:33 +0900

    main3

--- 05/timedulation/main.go
+++ 05/timedulation/main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	const (
 		Nanosecond  Duration = 1
-		Microsesond          = 1000
+		Microsesond          = 1000 * Nanosecond
+		Millisecond          = 1000 * Microsesond
+		Second               = 1000 * Millisecond
 	)
 }
