72fc2594413058a85164b2008cdacca490cf5081
Liberbell <liberbell@gmail.com>
2021-04-14 09:06:40 +0900

    main2

--- 05/defaulttype/main.go
+++ 05/defaulttype/main.go
@@ -7,4 +7,12 @@ func main() {
 	max := min + 5
 
 	fmt.Printf("Min: %d and Type is %T, Max: %d Type is %T\n", min, min, max, max)
+
+	i := 42
+	f := 3.14
+	b := true
+	s := "hello"
+	r := `A`
+
+	fmt.Printf("Type i: %T, Type f: %T, Type b: %T, Type s: %T, Type r: %T\n", i, f, b, s, r)
 }
