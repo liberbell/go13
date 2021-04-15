51c74f7c8dfe11606b5ec2d0bfc5f11dfa0014e9
Liberbell <liberbell@gmail.com>
2021-04-16 08:35:36 +0900

    main1

--- 05/iota/main.go
+++ 05/iota/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	const (
 		monday    = 0
@@ -10,4 +12,5 @@ func main() {
 		saturday  = 5
 		sunday    = 6
 	)
+	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
 }
