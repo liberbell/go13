37b48dcb602f9afd6508a0814b3f4ce849be60f0
Liberbell <liberbell@gmail.com>
2021-04-15 09:12:42 +0900

    main3

--- 05/feettometer/main.go
+++ 05/feettometer/main.go
@@ -9,15 +9,15 @@ import (
 func main() {
 	const (
 		feetInMeters float64 = 0.3048
-		feetInYards  float64 = feetInMeters / 0.91
+		feetInYards  float64 = feetInMeters / 0.9144
 	)
 
 	arg := os.Args[1]
 
 	feet, _ := strconv.ParseFloat(arg, 64)
 
-	meters := feet * 0.3048
-	yards := feet * 0.3333
+	meters := feet * feetInMeters
+	yards := feet * feetInYards
 
 	fmt.Printf("%g feet is %g meters\n", feet, meters)
 	fmt.Printf("%g feet is %g yards\n", feet, yards)
