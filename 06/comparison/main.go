8252248184744721dc1d5d8d4b07de495e342325
Liberbell <liberbell@gmail.com>
2021-04-17 21:07:08 +0900

    main3

--- 06/comparison/main.go
+++ 06/comparison/main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	speed := 100
 	fast := speed >= 80
+	slow := speed < 20
 
 	fmt.Printf("fast`s type is %T\n", fast)
 	fmt.Printf("going fast? %t\n", fast)
+	fmt.Printf("going slow? %t\n", fast)
 }
