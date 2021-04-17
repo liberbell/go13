c28a4e29653dc33f7e2e1858c3d63e3ebd3e9af2
Liberbell <liberbell@gmail.com>
2021-04-17 21:17:42 +0900

    main9

--- 06/comparison/main.go
+++ 06/comparison/main.go
@@ -18,4 +18,9 @@ func main() {
 	fmt.Printf("is it not 100 mph? %t\n", speed != 100)
 
 	fmt.Println(strings.Repeat("-", 25))
+
+	speedB := 150.5
+	faster := speedB > 100
+
+	fmt.Println("is the other car going faster?", faster)
 }
