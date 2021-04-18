571d5f4b560324d798bfcf183482ae6c29bbcf83
Liberbell <liberbell@gmail.com>
2021-04-19 08:52:36 +0900

    main3

--- 06/feettometer/main.go
+++ 06/feettometer/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("please tell me a value in feet")
+		return
 	}
+	arg := os.Args[1]
+	feet, _ := strconv.ParseFloat(arg, 64)
+	meters := feet * 0.3048
+
+	fmt.Printf("%g feet(s) is %g meters.\n", feet, meters)
 }
