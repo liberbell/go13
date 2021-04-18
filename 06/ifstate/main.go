e60c94190be11f0b58ddf2db8fb601144b8fe67c
Liberbell <liberbell@gmail.com>
2021-04-19 08:43:23 +0900

    main1

--- 06/ifstate/main.go
+++ 06/ifstate/main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	score, valid := 5, false
-	if score > 3 && valid {
+	// score, valid := 5, false
+	// if score > 3 && valid {
+	// 	fmt.Println("good")
+	// } else {
+	// 	fmt.Println("not good")
+	// }
+	score := 3
+
+	if score > 3 {
 		fmt.Println("good")
-	} else {
-		fmt.Println("not good")
+	} if score == 3 {
+		fmt.Println("")
 	}
 }
