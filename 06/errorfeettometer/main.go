46eca20f24c511fa1c38eaf35e241c7d0dcc2318
Liberbell <liberbell@gmail.com>
2021-04-21 14:17:53 +0900

    main2

--- 06/errorfeettometer/main.go
+++ 06/errorfeettometer/main.go
@@ -5,6 +5,13 @@ import (
 	"os"
 )
 
+const usage = `
+Feet to meters
+--------------
+This program converts feet into meters.
+usage:
+feet[feetsToConverts]`
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println(strings.TrimSapce(usage))
