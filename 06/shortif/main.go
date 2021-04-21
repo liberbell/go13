385da19161131b42b5aaa65899ff9fc5afac31a0
Liberbell <liberbell@gmail.com>
2021-04-22 08:55:28 +0900

    main3

--- 06/shortif/main.go
+++ 06/shortif/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	n, err := strconv.Atoi("42")
-	if err == nil {
-		fmt.Println("There was no err: n is ", n)
+
+	if n, err := strconv.Atoi("42"); err == nil {
+		fmt.Println("There was no err: n is", n)
 	}
 }
