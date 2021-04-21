517210fa2bdb78760838629bc9dacbcfe3e91b7f
Liberbell <liberbell@gmail.com>
2021-04-21 14:08:53 +0900

    main6

--- 06/errorvalue/main.go
+++ 06/errorvalue/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +9,9 @@ func main() {
 	// s := strconv.Itoa(42)
 	// fmt.Println(s)
 	// fmt.Printf("%T\n", s)
-	n, err := strconv.Atoi(os.Args[1])
-	fmt.Println("Convert number: ", n)
-	fmt.Println("Returned Err:  ", err)
+	// n, err := strconv.Atoi(os.Args[1])
+	// fmt.Println("Convert number: ", n)
+	// fmt.Println("Returned Err:  ", err)
+	age := os.Args[1]
+	n, err := strconv.Atoi(age)
 }
