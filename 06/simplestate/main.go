f69f709b43c2eda632162328713edc8373d12885
Liberbell <liberbell@gmail.com>
2021-04-22 09:09:46 +0900

    main5

--- 06/simplestate/main.go
+++ 06/simplestate/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: [number]")
-		return
+	// if len(os.Args) != 2 {
+	// 	fmt.Println("Usage: [number]")
+	// 	return
+	// }
+	// arg := os.Args[1]
+	// number, err := strconv.ParseFloat(arg, 64)
+	// if err != nil {
+	// 	fmt.Println("Error: Input number.")
+	// 	return
+	// }
+	// fmt.Printf("%g * 2 is %g.\n", number, number*2)
+
+	if arg := os.Args; len(arg) != 2 {
+		fmt.Println("Error: Input a number.")
 	}
-	arg := os.Args[1]
-	number, err := strconv.ParseFloat(arg, 64)
-	if err != nil {
-		fmt.Println("Error: Input number.")
-		return
-	}
-	fmt.Printf("%g * 2 is %g.\n", number, number*2)
 }
