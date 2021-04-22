8eafb58f11064b57a4d1198361c34de8fba728e5
Liberbell <liberbell@gmail.com>
2021-04-22 09:05:30 +0900

    main3

--- 06/simplestate/main.go
+++ 06/simplestate/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: [number]")
+		return
 	}
+	arg := os.Args[1]
+	number, err := strconv.ParseFloat(arg, 64)
+	if err != nil {
+		fmt.Println("Error: Input number.")
+		return
+	}
+	fmt.Printf("%g * 2 is %g.\n", number, number*2)
 }
