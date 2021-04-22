1e314f562d460b2a7ea39ac3f1e2902120116b1d
Liberbell <liberbell@gmail.com>
2021-04-23 08:30:22 +0900

    main4

--- 06/gotcha/main.go
+++ 06/gotcha/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var n int
 	if a := os.Args; len(a) != 2 {
 		fmt.Println("Input a number.")
+	} else if n, err := strconv.Atoi(a[1]); err != nil {
+		fmt.Printf("Cannot convert %q.\n", n)
 	}
 }
