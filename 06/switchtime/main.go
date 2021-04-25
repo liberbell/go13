f36a0c92c7933739c47ca68fd68136124bf680a0
Liberbell <liberbell@gmail.com>
2021-04-25 16:31:57 +0900

    main23

--- 06/switchtime/main.go
+++ 06/switchtime/main.go
@@ -10,9 +10,11 @@ func main() {
 	fmt.Println("Current hour is: ", nowHour)
 
 	switch {
-	case nowHour >= 6, nowHour < 12:
+	case nowHour >= 18:
+		fmt.Println("Good night.")
+	case nowHour >= 12:
+		fmt.Println("Good afternoon.")
+	default:
 		fmt.Println("Good morning.")
-	case nowHour >= 12, nowHour < 17:
-		fmt.Println("Good evening.")
 	}
 }
