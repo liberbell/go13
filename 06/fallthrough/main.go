0c7cc652a6b79ed00ef8d864195446da12c20a1b
Liberbell <liberbell@gmail.com>
2021-04-25 16:12:55 +0900

    main2

--- 06/fallthrough/main.go
+++ 06/fallthrough/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	i := 100
+
+	switch {
+	case i > 100:
+		fmt.Println("Big positive number.")
+	case i > 0:
+		fmt.Println("positive")
+	default:
+		fmt.Println("number")
+	}
 }
