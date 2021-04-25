dddbe62633654b73bef69626296275d6b24ff087
Liberbell <liberbell@gmail.com>
2021-04-25 16:04:57 +0900

    main2

--- 06/switchbool/main.go
+++ 06/switchbool/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	i := 10
+	i := 0
 
 	switch {
 	case i > 0:
 		fmt.Println("positive")
 	case i < 0:
 		fmt.Println("negative")
+	default:
+		fmt.Println("Zero")
 	}
 }
