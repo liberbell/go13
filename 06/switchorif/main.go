db6bea1559a2d3d55b076f279ac5c45c673bdce3
Liberbell <liberbell@gmail.com>
2021-04-26 09:07:14 +0900

    main14

--- 06/switchorif/main.go
+++ 06/switchorif/main.go
@@ -10,30 +10,30 @@ func main() {
 		fmt.Println("Give a month name.")
 		return
 	}
-	// m := os.Args[1]
+	m := os.Args[1]
 
-	// if m == "Dec" || m == "Jan" || m == "Feb" {
-	// 	fmt.Println("Winter")
-	// } else if m == "Mar" || m == "Apr" || m == "May" {
-	// 	fmt.Println("Spring")
-	// } else if m == "Jun" || m == "Jly" || m == "Aug" {
-	// 	fmt.Println("Summer")
-	// } else if m == "Sep" || m == "Oct" || m == "Nov" {
-	// 	fmt.Println("Autumn")
-	// } else {
-	// 	fmt.Printf("%q is wrong month.\n", m)
-	// }
-
-	switch m := os.Args[1]; m {
-	case "Dec", "Jan", "Feb":
-		fmt.Println("winter")
-	case "Mar", "Apr", "May":
+	if m == "Dec" || m == "Jan" || m == "Feb" {
+		fmt.Println("Winter")
+	} else if m == "Mar" || m == "Apr" || m == "May" {
 		fmt.Println("Spring")
-	case "Jun", "Jly", "Aug":
+	} else if m == "Jun" || m == "Jly" || m == "Aug" {
 		fmt.Println("Summer")
-	case "Sep", "Oct", "Nov":
+	} else if m == "Sep" || m == "Oct" || m == "Nov" {
 		fmt.Println("Autumn")
-	default:
+	} else {
 		fmt.Printf("%q is wrong month.\n", m)
 	}
+
+	// switch m := os.Args[1]; m {
+	// case "Dec", "Jan", "Feb":
+	// 	fmt.Println("winter")
+	// case "Mar", "Apr", "May":
+	// 	fmt.Println("Spring")
+	// case "Jun", "Jly", "Aug":
+	// 	fmt.Println("Summer")
+	// case "Sep", "Oct", "Nov":
+	// 	fmt.Println("Autumn")
+	// default:
+	// 	fmt.Printf("%q is wrong month.\n", m)
+	// }
 }
