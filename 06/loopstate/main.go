236342dc3ed1fc5b3ef1977cf03109f869c31904
Liberbell <liberbell@gmail.com>
2021-04-27 09:00:11 +0900

    main3

--- 06/loopstate/main.go
+++ 06/loopstate/main.go
@@ -5,11 +5,15 @@ import "fmt"
 func main() {
 	var sum int
 
-	sum++
-	sum += 2
-	sum += 3
-	sum += 4
-	sum += 5
+	// sum++
+	// sum += 2
+	// sum += 3
+	// sum += 4
+	// sum += 5
 
-	fmt.Println(sum)
+	// fmt.Println(sum)
+
+	for i := 1; i <= 1000; i++ {
+		fmt.Println(i)
+	}
 }
