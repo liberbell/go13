89af2c1e3bfa5dd937cfdd8fef0bb59062d04ac4
Liberbell <liberbell@gmail.com>
2021-04-28 08:45:11 +0900

    main1

--- 06/loopmultipletable/main.go
+++ 06/loopmultipletable/main.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+const max = 5
+
 func main() {
 	fmt.Printf("%5s", "X")
-	for i := 0; i <= 5; i++ {
+	for i := 0; i <= max; i++ {
 		fmt.Printf("%5d", i)
 	}
+	fmt.Println("")
 }
