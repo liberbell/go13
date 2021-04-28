d569f66df1218fa4fca052244d9274c98b447d54
Liberbell <liberbell@gmail.com>
2021-04-28 09:09:01 +0900

    main5

--- 06/loopslice/main.go
+++ 06/loopslice/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"strings"
 )
 
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("%q\n", os.Args[i])
-	}
+	// for i := 1; i < len(os.Args); i++ {
+	// 	fmt.Printf("%q\n", os.Args[i])
+	// }
+
+	words := strings.Fields("lazy cat jumps again and again and again")
+
 }
