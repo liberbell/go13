f3460330083151d6d9e93c6335a363d89a22e269
Liberbell <liberbell@gmail.com>
2021-04-28 09:28:38 +0900

    main7

--- 06/looprange/main.go
+++ 06/looprange/main.go
@@ -9,10 +9,7 @@ func main() {
 	// for i := 1; i < len(os.Args); i++ {
 	// 	fmt.Printf("%q\n", os.Args[i])
 	// }
-	for i, v := range os.Args {
-		if i == 0 {
-			continue
-		}
+	for _, v := range os.Args[1:] {
 		fmt.Printf("%q\n", v)
 	}
 }
