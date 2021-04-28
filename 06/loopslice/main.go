7b85b7a21f49d7cb9c745ed7fe27485da851961c
Liberbell <liberbell@gmail.com>
2021-04-28 09:12:10 +0900

    main7

--- 06/loopslice/main.go
+++ 06/loopslice/main.go
@@ -13,6 +13,6 @@ func main() {
 	words := strings.Fields("lazy cat jumps again and again and again")
 
 	for j := 0; j < len(words); j++ {
-		fmt.Printf("#%-2d: %q\n", j+1)
+		fmt.Printf("#%-2d: %q\n", j+1, words[j])
 	}
 }
