cc13a996a490ac53db2d539a0dd88cdedbd75048
Liberbell <liberbell@gmail.com>
2021-04-29 17:14:48 +0900

    main6

--- 06/randamization/main.go
+++ 06/randamization/main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	rand.Seed(10)
+
 	guess := 10
 
 	for n := 0; n != guess; {
 		n = rand.Intn(guess + 1)
-		fmt.Printf("%d", n)
+		fmt.Printf("%d ", n)
 	}
 	fmt.Println()
 }
