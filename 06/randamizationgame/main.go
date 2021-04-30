abd06a479bcd430443a6904767ecb658ef45bd50
Liberbell <liberbell@gmail.com>
2021-04-30 14:10:57 +0900

    main6

--- 06/randamizationgame/main.go
+++ 06/randamizationgame/main.go
@@ -6,11 +6,13 @@ import (
 	"time"
 )
 
+const maxTurns = 3
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	guess := 10
 
-	for turn := 0; turn < 5; turn++ {
+	for turn := 0; turn < maxTurns; turn++ {
 		n := rand.Intn(guess + 1)
 
 		if n == guess {
