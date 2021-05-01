0a3414110c602dfe156987d6f13f0449e2e8b1df
Liberbell <liberbell@gmail.com>
2021-05-01 10:19:47 +0900

    main4

--- 06/loopword/main.go
+++ 06/loopword/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +10,9 @@ const corpus = "" + "lazy cat jumps again and again and again"
 
 func main() {
 	words := strings.Fields(corpus)
+	query := os.Args[1:]
+
+	for _, q := range query {
+		fmt.Println(q)
+	}
 }
