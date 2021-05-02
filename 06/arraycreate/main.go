7de877b32648f3d36a16bb94a6d97a617329e270
Liberbell <liberbell@gmail.com>
2021-05-02 21:00:05 +0900

    main11

--- 06/arraycreate/main.go
+++ 06/arraycreate/main.go
@@ -16,8 +16,8 @@ func main() {
 	books[2] = "Everythingship"
 	books[3] = books[0] + " 2nd Edition"
 
-	fmt.Printf("books: %T\n", books)
-	fmt.Println("books: ", books)
-	fmt.Printf("books: %q\n", books)
+	// fmt.Printf("books: %T\n", books)
+	// fmt.Println("books: ", books)
+	// fmt.Printf("books: %q\n", books)
 	fmt.Printf("books: %#v\n", books)
 }
