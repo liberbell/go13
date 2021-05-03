d507dd402099e42392e40ea36b37b5bc2da1460f
Liberbell <liberbell@gmail.com>
2021-05-03 13:08:21 +0900

    main3

--- 06/arraymoodly/main.go
+++ 06/arraymoodly/main.go
@@ -9,5 +9,11 @@ func main() {
 	arg := os.Args[1:]
 	if len(arg) != 1 {
 		fmt.Println("Usage: [yuar name]")
+		return
+	}
+	name := arg[0]
+	moods := [...]string{
+		"happy", "good", "awesome",
+		"sad", "bad", "terrible",
 	}
 }
