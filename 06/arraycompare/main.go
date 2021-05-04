77a3f9a42e2e895a914269ebba74d8ea6844a929
Liberbell <liberbell@gmail.com>
2021-05-04 22:29:26 +0900

    main5

--- 06/arraycompare/main.go
+++ 06/arraycompare/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var (
-		blue = [3]int{6, 9, 3}
-		red  = [3]int{6, 3, 9}
+		blue = [3]int{6, 9}
+		red  = [3]int{6, 9, 3}
 	)
 
 	fmt.Printf("blue bookcase: %v\n", blue)
