e498c46e2ad9a630ea85535cdda9298d7bb5bbfb
Liberbell <liberbell@gmail.com>
2021-05-04 22:50:57 +0900

    main4

--- 06/arraycopied/main.go
+++ 06/arraycopied/main.go
@@ -5,14 +5,16 @@ import (
 )
 
 func main() {
-	var blue = [3]int{6, 9, 3}
+	// var blue = [3]int{6, 9, 3}
 
-	red := blue
+	// red := blue
 
-	fmt.Println(blue)
-	fmt.Println(red)
+	// fmt.Println(blue)
+	// fmt.Println(red)
 
-	red[1] = 10
-	fmt.Println(blue)
-	fmt.Println(red)
+	// red[1] = 10
+	// fmt.Println(blue)
+	// fmt.Println(red)
+	prev := [3]string{"Kafka's Revenge", "Stay Golden", "Everythingship"}
+	fmt.Printf("last year: \n%#v\n", prev)
 }
