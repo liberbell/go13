5daf72d42124753c133e2fb5f76e390d1af04e95
Liberbell <liberbell@gmail.com>
2021-05-05 13:13:15 +0900

    main7

--- 06/arraymulti/main.go
+++ 06/arraymulti/main.go
@@ -13,12 +13,13 @@ func main() {
 	// const N = float64(len(students1) * 2)
 	// fmt.Printf("Avg Grade: %g\n", sum/N)
 	students := [2][3]float64{
-		[3]float64{5, 6, 1}
-		[3]float64{9, 8, 1}
+		[3]float64{5, 6, 1},
+		[3]float64{9, 8, 1},
 	}
 	var sum float64
 	sum += students[0][0] + students[0][1] + students[0][2]
 	sum += students[1][0] + students[1][1] + students[1][2]
 
 	const N float64(len(students) * len(students[0]))
+	fmt.Printf("Avg Grade: %g\n", sum/N)
 }
