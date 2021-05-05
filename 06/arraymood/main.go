827baf9f092e5d8cf9b5a1da483d1439b9d8939d
Liberbell <liberbell@gmail.com>
2021-05-05 13:24:29 +0900

    main4

--- 06/arraymood/main.go
+++ 06/arraymood/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	positives := [3]string{"good!", "awesome!", "happy!"}
-	negatives := [3]string{"bad.", "happy.", "terrible."}
+	// positives := [3]string{"good!", "awesome!", "happy!"}
+	// negatives := [3]string{"bad.", "happy.", "terrible."}
 
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: [name][positive|negative]")
