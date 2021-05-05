44472c970bdeba697ba33bede50993a0d923f25c
Liberbell <liberbell@gmail.com>
2021-05-05 13:06:32 +0900

    main4

--- 06/arraymulti/main.go
+++ 06/arraymulti/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	students1 := [3]float64{5, 6, 1}
-	students2 := [3]float64{9, 8, 4}
+	// students1 := [3]float64{5, 6, 1}
+	// students2 := [3]float64{9, 8, 4}
 
-	var sum float64
-	sum += students1[0] + students1[1] + students1[2]
-	sum += students2[0] + students2[1] + students2[2]
+	// var sum float64
+	// sum += students1[0] + students1[1] + students1[2]
+	// sum += students2[0] + students2[1] + students2[2]
 
-	const N = float64(len(students1) * 2)
-	fmt.Printf("Avg Grade: %g\n", sum/N)
+	// const N = float64(len(students1) * 2)
+	// fmt.Printf("Avg Grade: %g\n", sum/N)
+	student := [2][3]float64{
+		[3]float64{5, 6, 1}
+		[3]float64{9, 8, 1}
+	}
 }
