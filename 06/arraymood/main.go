06fd70553942e689436811dac93c041e59c94049
Liberbell <liberbell@gmail.com>
2021-05-07 08:38:52 +0900

    main3

--- 06/arraymood/main.go
+++ 06/arraymood/main.go
@@ -16,6 +16,8 @@ func main() {
 		{"bad", "sad", "terrible"},
 	}
 
+	var mi int
+
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: [your name] [positive|negative]")
 		return
@@ -23,7 +25,12 @@ func main() {
 	name, feeling := os.Args[1], os.Args[2]
 	// feeling := os.Args[2]
 
+	if feeling != "positive" {
+		mi = 1
+	}
+
 	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(moods))
 
 	if feeling != "positive" && feeling != "negative" {
 		// fmt.Printf("%s %s\n", name, feeling)
@@ -35,4 +42,6 @@ func main() {
 		n := rand.Intn(len(negatives))
 		fmt.Printf("%s has a %s feelings.\n", name, negatives[n])
 	}
+
+	fmt.Printf("%s feels %s\n", name, moods[n])
 }
