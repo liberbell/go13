3d2f4bb05dd7c2830d5f068397f7c0d543606975
Liberbell <liberbell@gmail.com>
2021-05-07 09:22:26 +0900

    main3

--- 06/unnametype/main.go
+++ 06/unnametype/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	blue :=
 		[5]int{6, 9, 3, 2, 1}
 	red :=
 		[5]int{6, 9, 3, 2, 1}
+
+	fmt.Print("Are they equal")
+	if blue == red {
+		fmt.Println("They are equal.")
+	}
 }
