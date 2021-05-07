3bec1251c9fb72335dece60b96d366a80ca5bed0
Liberbell <liberbell@gmail.com>
2021-05-07 09:27:42 +0900

    main6

--- 06/unnametype/main.go
+++ 06/unnametype/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	type bookcase [5]int
+	type (
+		bookcase [5]int
+		cabinet  [5]int
+	)
 
 	blue :=
 		// [5]int{6, 9, 3, 2, 4}
 		bookcase{6, 9, 3, 2, 4}
 	red :=
-		[5]int{6, 9, 3, 2, 1}
+		// [5]int{6, 9, 3, 2, 1}
+		cabinet{6, 9, 3, 2, 1}
 
 	fmt.Print("Are they equal?\n")
 	if blue == red {
