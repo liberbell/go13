cfaae9c82e58b28df2e93a5b55cff6cb02cffff2
Liberbell <liberbell@gmail.com>
2021-05-07 09:05:50 +0900

    main8

--- 06/arraykeys/main.go
+++ 06/arraykeys/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 const (
-	ETH = iota
+	ETH = 9 - iota
 	WAN
+	ICX
 )
 
 func main() {
 	rates := [...]float64{
 		ETH: 25.5,
 		WAN: 120.5,
+		ICX: 20,
 	}
 	fmt.Printf("1 BTS is %g ETH\n", rates[ETH])
 	fmt.Printf("1 BTS is %g WAN\n", rates[WAN])
