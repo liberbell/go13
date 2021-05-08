2f9e8c1ba4d1aca0c9bef3837610b0e375b92ca0
Liberbell <liberbell@gmail.com>
2021-05-08 10:32:40 +0900

    main13

--- 07/retroclock/main.go
+++ 07/retroclock/main.go
@@ -91,10 +91,15 @@ func main() {
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	fmt.Println(digits[0])
-	for _, digit := range digits {
-		for _, line := range digit {
-			fmt.Println(line)
+	// fmt.Println(digits[0])
+	// for _, digit := range digits {
+	// 	for _, line := range digit {
+	// 		fmt.Println(line)
+	// 	}
+	// }
+	for line := range digits[0] {
+		for digit := range digits {
+			fmt.Println(digits[digit][line])
 		}
 	}
 }
