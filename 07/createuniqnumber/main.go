909c4e5e28e46263af694d5439719d3229773bad
Liberbell <liberbell@gmail.com>
2021-05-11 09:14:47 +0900

    main5

--- 07/createuniqnumber/main.go
+++ 07/createuniqnumber/main.go
@@ -12,9 +12,16 @@ func main() {
 	const max = 5
 	var uniqes [max]int
 
+loop:
 	for found := 0; found < max; found++ {
 		n := rand.Intn(max)
 
 		fmt.Print(n, " ")
+
+		for _, u := range uniqes {
+			if u == n {
+				continue loop
+			}
+		}
 	}
 }
