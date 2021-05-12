b88bfbb55c55297b2203dc64a7dbda47083e847c
Liberbell <liberbell@gmail.com>
2021-05-12 09:07:04 +0900

    main6

--- 07/sliceappend/main.go
+++ 07/sliceappend/main.go
@@ -6,7 +6,7 @@ func main() {
 	var todo []string
 
 	todo = append(todo, "sing")
-	todo = append(todo, "run")
+	todo = append(todo, "run", "code", "play")
 	todo = append(todo, "code")
 	todo = append(todo, "play")
 
