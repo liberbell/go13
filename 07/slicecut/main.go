60405544915082352c482f79ceb7fa806e6bf4dd
Liberbell <liberbell@gmail.com>
2021-05-13 09:57:35 +0900

    main4

--- 07/slicecut/main.go
+++ 07/slicecut/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	msg := []string{"h", "e", "l", "l", "o"}
-	fmt.Println(msg)
-	fmt.Println(msg[0:4])
-	fmt.Println(msg[:4])
-	fmt.Println(append(msg[:4], "!"))
+	// msg := []string{"h", "e", "l", "l", "o"}
+	// fmt.Println(msg)
+	// fmt.Println(msg[0:4])
+	// fmt.Println(msg[:4])
+	// fmt.Println(append(msg[:4], "!"))
+
+	items := []string{
+		"pacman", "mario", "tetris", "doom",
+		"galaga", "frogger", "asteroids", "simcity",
+	}
 }
