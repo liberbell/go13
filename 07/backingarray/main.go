4f34e3abd90ac4fe25d3c16926a61e262c600eff
Liberbell <liberbell@gmail.com>
2021-05-14 09:03:57 +0900

    main4

--- 07/backingarray/main.go
+++ 07/backingarray/main.go
@@ -1,16 +1,18 @@
 package main
 
-import "fmt"
+import s "github.com/inancgumus/prettyslice"
 
 func main() {
 	agesArray := [3]int{35, 15, 25}
-	age := agesArray[0:3]
+	// age := agesArray[0:3]
 
-	fmt.Println(agesArray)
-	fmt.Println(age)
+	// fmt.Println(agesArray)
+	// fmt.Println(age)
 
-	age[0] = 100
-	fmt.Println(agesArray)
-	fmt.Println(age)
+	// age[0] = 100
+	// fmt.Println(agesArray)
+	// fmt.Println(age)
+
+	s.Show(agesArray)
 
 }
