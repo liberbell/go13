eda7e8f0d31bba9600c38dca0102c7a044db7eec
Liberbell <liberbell@gmail.com>
2021-05-17 08:38:22 +0900

    main3

--- 07/slicegrows/main.go
+++ 07/slicegrows/main.go
@@ -1,9 +1,21 @@
 package main
 
-import s "github.com/inancgumus/prettyslice"
+import (
+	s "github.com/inancgumus/prettyslice"
+	"github.com/inancgumus/screen"
+)
 
 func main() {
 	s.PrintBacking = true
 	s.MaxPerLine = 30
 	s.Width = 150
+
+	var nums []int
+
+	screen.Clear()
+	for cap(nums) <= 128 {
+		screen.MoveTopLeft()
+
+		s.Show("nums", nums)
+	}
 }
