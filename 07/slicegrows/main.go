3677aca729ca0044c44824f968d06a1dcc319178
Liberbell <liberbell@gmail.com>
2021-05-17 08:45:32 +0900

    main6

--- 07/slicegrows/main.go
+++ 07/slicegrows/main.go
@@ -24,4 +24,13 @@ func main() {
 
 		time.Sleep(time.Second / 4)
 	}
+
+	ages, oldCap := []int{1}, 1.
+
+	for len(ages) < 5e5 {
+		ages = append(ages, 1)
+
+		c := float64(cap(ages))
+		if c := oldCap
+	}
 }
