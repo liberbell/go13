c220d66b7ba57b8dc4d5a7330fb3dbf464611954
Liberbell <liberbell@gmail.com>
2021-05-16 13:37:48 +0900

    main4

--- 07/appendfunc/main.go
+++ 07/appendfunc/main.go
@@ -17,6 +17,9 @@ func main() {
 	nums = append(nums, 4)
 	s.Show("no allocation", nums)
 
+	nums = append(nums, nums[2:]...)
+	s.Show("nums <- 7, 9", nums)
+
 	nums = append(nums[:2], 7, 9)
 	s.Show("nums[:2] <- 7, 9", nums)
 }
