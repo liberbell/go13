0f5893eff8b9fe7ed9af1b2a7f7e3f31572a4b25
Liberbell <liberbell@gmail.com>
2021-05-18 08:44:20 +0900

    main3

--- 07/preallocation/main.go
+++ 07/preallocation/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/inancgumus/prettyslice"
+)
 
 func main() {
 	s := make([]int, 0, 5)
 	fmt.Println(s)
 	s = append(s, 42)
 	fmt.Println(s)
+
+	prettyslice.PrintBacking = true
+	prettyslice.MaxPerLine = 10
+
 }
