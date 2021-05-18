8f85f4247d6c170dea8573a8e29c46ecad36bf9e
Liberbell <liberbell@gmail.com>
2021-05-18 09:12:32 +0900

    main4

--- 07/copyslice/main.go
+++ 07/copyslice/main.go
@@ -5,10 +5,11 @@ import "github.com/inancgumus/prettyslice"
 func main() {
 	evens := []int{2, 4}
 	odds := []int{1, 3, 5}
-	numbers := copy(evens, odds)
 
 	prettyslice.Show("evens", evens)
 	prettyslice.Show("odds", odds)
+
+	numbers := copy(evens, odds)
 
 	prettyslice.Show("numbers", numbers)
 }
