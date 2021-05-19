0b32bc4ddd2b2d91ccd10cc47fc9a116421fea90
Liberbell <liberbell@gmail.com>
2021-05-19 13:21:07 +0900

    main3

--- 07/multidimention/main.go
+++ 07/multidimention/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	spendings := [][]int{
 		{200, 100},
 		{500},
 		{50, 25, 75},
+	}
+
+	for i, daily := range spendings {
+		fmt.Println(i+1, daily)
 	}
 }
