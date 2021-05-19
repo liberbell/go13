10040963cee7d58ab65e6e93789706b82229c2b0
Liberbell <liberbell@gmail.com>
2021-05-19 12:55:17 +0900

    main2

--- 07/copyslice/main.go
+++ 07/copyslice/main.go
@@ -6,15 +6,15 @@ import (
 
 func main() {
 	s.PrintBacking = true
-	evens := []int{2, 4}
-	odds := []int{1, 3, 5}
+	// evens := []int{2, 4}
+	// odds := []int{1, 3, 5}
 
-	s.Show("evens", evens)
-	s.Show("odds", odds)
+	// s.Show("evens", evens)
+	// s.Show("odds", odds)
 
-	numbers := copy(evens, odds)
+	// numbers := copy(evens, odds)
 
-	s.Show("numbers", numbers)
+	// s.Show("numbers", numbers)
 
 	data := []int{10, 25, 30, 50}
 	s.Show("Probabilies", data)
