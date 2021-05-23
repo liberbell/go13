966ef7a10ab556776165916564f20a6490a4b94a
Liberbell <liberbell@gmail.com>
2021-05-23 17:40:00 +0900

    main6

--- 09/bounceball/main.go
+++ 09/bounceball/main.go
@@ -22,6 +22,15 @@ func main() {
 
 	board[0][0] = true
 
+	board[12][2] = true
+	board[16][2] = true
+	board[14][4] = true
+	board[10][6] = true
+	board[18][6] = true
+	board[12][7] = true
+	board[14][7] = true
+	board[16][7] = true
+
 	// draw board
 	for y := range board[0] {
 		for x := range board {
