e31d9c49a820af8f134284dedf8a4e11f49f80c8
Liberbell <liberbell@gmail.com>
2021-05-24 09:08:21 +0900

    main5

--- 10/basicbytes/main.go
+++ 10/basicbytes/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	start, stop := 'A', 'Z'
 	fmt.Println(start, stop)
 
-	fmt.Printf("%s\n%s\n", "literal")
+	fmt.Printf("%s\n%s\n", "literal", strings.Repeat("-", 10))
 
 	for n := start; n <= stop; n++ {
 		fmt.Printf("%c -> %[1]\nd ", n)
