29b2e3de330aab11175d37793f5fcdf8c8793e3c
Liberbell <liberbell@gmail.com>
2021-05-24 09:07:24 +0900

    main4

--- 10/basicbytes/main.go
+++ 10/basicbytes/main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	start, stop := 'A', 'Z'
 	fmt.Println(start, stop)
+
+	fmt.Printf("%s\n%s\n", "literal")
+
+	for n := start; n <= stop; n++ {
+		fmt.Printf("%c -> %[1]\nd ", n)
+	}
 }
