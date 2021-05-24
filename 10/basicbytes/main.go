ff620da5d1e20b289f4e0583d7cb9bd68804f3c9
Liberbell <liberbell@gmail.com>
2021-05-24 09:09:53 +0900

    main6

--- 10/basicbytes/main.go
+++ 10/basicbytes/main.go
@@ -9,7 +9,7 @@ func main() {
 	start, stop := 'A', 'Z'
 	fmt.Println(start, stop)
 
-	fmt.Printf("%s\n%s\n", "literal", strings.Repeat("-", 10))
+	fmt.Printf("%a %s\n%s\n", "literal", "dec", strings.Repeat("-", 10))
 
 	for n := start; n <= stop; n++ {
 		fmt.Printf("%c -> %[1]d\n", n)
