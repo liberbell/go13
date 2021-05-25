2c6350e320a36a99f48aa63ec921f3d4c2c92888
Liberbell <liberbell@gmail.com>
2021-05-25 14:16:41 +0900

    main5

--- 10/basicbytes/main.go
+++ 10/basicbytes/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	start, stop := 'A', 'Z'
+	// start, stop := 'A', 'Z'
+	var start, stop int
+
+	if args := os.Args[1:]; len(args) == 2 {
+		start, _ = strconv.Atoi(args[0])
+		stop, _ = strconv.Atoi(args[1])
+	}
+
 	fmt.Println(start, stop)
 
 	fmt.Printf("%-10s %-10s %-10s %-12s\n%s\n", "literal", "dec", "hex", "encoded", strings.Repeat("-", 45))
