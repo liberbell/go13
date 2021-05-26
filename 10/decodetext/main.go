a2d9d98f3cac38829f1fe806f16705bead28a04c
Liberbell <liberbell@gmail.com>
2021-05-26 09:16:24 +0900

    main6

--- 10/decodetext/main.go
+++ 10/decodetext/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	text := `
 出力は5V/3A、9V/3A、15V/3A、20V/2.25A、PPS出力が3.3～16V=3A、3.3～21V=2.25Aなどとなっている。`
 
 	for i := 0; i < len(text); i++ {
-		r := rune(text[i])
+		r, _ := utf8.DecodeRuneInString(text)
 		fmt.Printf("%c", r)
 	}
 
