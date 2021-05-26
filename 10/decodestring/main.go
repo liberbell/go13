4f9530b68508af8a51d96dc46ac04ef6187d8b6a
Liberbell <liberbell@gmail.com>
2021-05-27 08:42:25 +0900

    main3

--- 10/decodestring/main.go
+++ 10/decodestring/main.go
@@ -5,4 +5,12 @@ import "fmt"
 func main() {
 	word := []byte("力は5V/3A")
 	fmt.Printf("%s = % [1]x\n", word)
+
+	var size int
+	for i := range string(word) {
+		if i > 0 {
+			size = 1
+			break
+		}
+	}
 }
