d62805fc34c02f143173fa12a5ec351888ac5eab
Liberbell <liberbell@gmail.com>
2021-05-26 09:11:19 +0900

    main4

--- 10/decodetext/main.go
+++ 10/decodetext/main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func main() {
 	text := `
-	出力は5V/3A、9V/3A、15V/3A、20V/2.25A、PPS出力が3.3～16V=3A、3.3～21V=2.25Aなどとなっている。
-	`
+出力は5V/3A、9V/3A、15V/3A、20V/2.25A、PPS出力が3.3～16V=3A、3.3～21V=2.25Aなどとなっている。`
 
 	for i := 0; i < len(text); i++ {
 		fmt.Printf("%c", text[i])
