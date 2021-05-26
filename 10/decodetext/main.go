57e2a00f51b5b4ac98a3820a702acc33f0810d7f
Liberbell <liberbell@gmail.com>
2021-05-26 09:10:24 +0900

    main2

--- 10/decodetext/main.go
+++ 10/decodetext/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main()  {
-	text := 
+import "fmt"
+
+func main() {
+	text := `
+	出力は5V/3A、9V/3A、15V/3A、20V/2.25A、PPS出力が3.3～16V=3A、3.3～21V=2.25Aなどとなっている。
+	`
+
+	for i := 0; i < len(text); i++ {
+		fmt.Printf("%c\n", text[i])
+	}
+
 }
