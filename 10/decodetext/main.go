f2832b1d35451dfdd70c078ec8dee00085b25ed2
Liberbell <liberbell@gmail.com>
2021-05-27 08:35:57 +0900

    main7

--- 10/decodetext/main.go
+++ 10/decodetext/main.go
@@ -9,8 +9,8 @@ func main() {
 	text := `So i come from Python, and they encourages you to program with readable not-that-
 I’m 出力は5V/3A、9V/3A、15V/3A、20V/2.25A、PPS出力が3.3～16V=3A、3.3～21V=2.25Aなどとなっている。`
 
-	for i := 0; i < len(text); {
-		r, size := utf8.DecodeRuneInString(text[i:])
+	for _, r; range text {
+		// r, size := utf8.DecodeRuneInString(text[i:])
 		fmt.Printf("%c", r)
 
 		i += size
