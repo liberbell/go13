26e24b34d102bc6f2f0f09da53230ad349d00210
Liberbell <liberbell@gmail.com>
2021-05-27 08:45:39 +0900

    main6

--- 10/decodestring/main.go
+++ 10/decodestring/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
 	word := []byte("力は5V/3A")
 	fmt.Printf("%s = % [1]x\n", word)
 
-	var size int
-	for i := range string(word) {
-		if i > 0 {
-			size = 1
-			break
-		}
-	}
+	// var size int
+	// for i := range string(word) {
+	// 	if i > 0 {
+	// 		size = 1
+	// 		break
+	// 	}
+	// }
+
+	_, size := utf8.DecodeRune(word)
+
 	copy(word[:size], bytes.ToUpper(word[:size]))
 	fmt.Printf("%s = % [1]x\n", word)
 }
