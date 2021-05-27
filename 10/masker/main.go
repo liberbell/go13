d30b12f51b5507395975fdc3be0cbaa90dd7a137
Liberbell <liberbell@gmail.com>
2021-05-28 08:55:06 +0900

    main2

--- 10/masker/main.go
+++ 10/masker/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Get and check the input
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("gimme something to mask!")
+	}
+}
