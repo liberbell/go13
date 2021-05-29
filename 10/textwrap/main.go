dc83c806b8b5e1759dfdb6af64d3704ed8b52ab9
Liberbell <liberbell@gmail.com>
2021-05-29 13:42:06 +0900

    main15

--- 10/textwrap/main.go
+++ 10/textwrap/main.go
@@ -5,14 +5,14 @@ import "fmt"
 func main() {
 	const text = `チャットへようこそ！ご自身のプライバシーを守るとともに、YouTube のコミュニティ ガイドラインを遵守することを忘れないでください。Microsoft本社主催のMicrosoft Build 2021 が米国時間5月25日～27日にかけて開催されます。本イベントではBuildアップデートを含めた各サービスの最新アップデートをJAZUGの枠を超えて様々なコミュニティの方にお話ししていただきます！`
 
-	const maxwidth = 40
+	const maxWidth = 40
 
 	var lw int
 
 	for _, r := range text {
 		fmt.Printf("%c", r)
 
-		if lw > maxwidth {
+		if lw++; lw > maxWidth {
 			fmt.Println()
 		}
 	}
