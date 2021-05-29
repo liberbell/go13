1add2aedff6cd63d027c3e7845916ed85df05822
Liberbell <liberbell@gmail.com>
2021-05-29 13:32:10 +0900

    main2

--- 10/textwrap/main.go
+++ 10/textwrap/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+	const text = `チャットへようこそ！ご自身のプライバシーを守るとともに、YouTube のコミュニティ ガイドラインを遵守することを忘れないでください。`
+
+	for _, r := range text {
+		fmt.Printf("%c", r)
+	}
+}
