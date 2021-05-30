6951839044f5c234a10b9f4ffe83a67c22be2527
Liberbell <liberbell@gmail.com>
2021-05-30 13:47:16 +0900

    main3

--- 10/mapscollect/main.go
+++ 10/mapscollect/main.go
@@ -13,4 +13,14 @@ func main() {
 	}
 
 	query := args[0]
+
+	english := []string{"good", "great", "perfect"}
+	japanese := []string{"よい", "すげえ", "完璧"}
+
+	for i, w := range english {
+		if query == w {
+			fmt.Printf("%q means %q\n", w, japanese[i])
+			return
+		}
+	}
 }
