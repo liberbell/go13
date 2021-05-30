76505d3e66ef246545ebf9c19f6cf8db04b98699
Liberbell <liberbell@gmail.com>
2021-05-30 13:55:27 +0900

    main8

--- 10/mapscollect/main.go
+++ 10/mapscollect/main.go
@@ -14,17 +14,21 @@ func main() {
 	// query := args[0]
 	var dict map[string]string
 
+	key := "good"
+	value := dict[key]
+	fmt.Printf("%q means %v\n, var)
+
 	fmt.Printf("Zero value: %v\n", dict)
-	fmt.Printf("# of keys : %d\n, var)
+	// fmt.Printf("# of keys : %d\n, var)
 
-	english := []string{"good", "great", "perfect"}
-	japanese := []string{"よい", "すげえ", "完璧"}
+	// english := []string{"good", "great", "perfect"}
+	// japanese := []string{"よい", "すげえ", "完璧"}
 
-	for i, w := range english {
-		if query == w {
-			fmt.Printf("%q means %q\n", w, japanese[i])
-			return
-		}
-	}
-	fmt.Printf("%q not found\n", query)
+	// for i, w := range english {
+	// 	if query == w {
+	// 		fmt.Printf("%q means %q\n", w, japanese[i])
+	// 		return
+	// 	}
+	// }
+	// fmt.Printf("%q not found\n", query)
 }
