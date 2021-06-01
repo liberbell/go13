7561ce7013b74f657ef58b4d0d0c3e343c513bff
Liberbell <liberbell@gmail.com>
2021-06-02 08:42:33 +0900

    main4

--- 11/scaninput/main.go
+++ 11/scaninput/main.go
@@ -8,10 +8,17 @@ import (
 
 func main() {
 	in := bufio.NewScanner(os.Stdin)
-	for in.Scan() {
-		fmt.Println("Scanned text: ", in.Text())
-	}
 
+	// for in.Scan() {
+	// 	fmt.Println("Scanned text: ", in.Text())
+	// }
 	// fmt.Println("Sccaned text: ", in.Text())
 	// fmt.Println("Sccaned text: ", in.Bytes())
+
+	var lines int
+
+	for in.Scan() {
+		lines++
+	}
+	fmt.Printf("There are %d line(s)\n", lines)
 }
