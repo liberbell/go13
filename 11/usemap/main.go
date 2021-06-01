fb6727de7b277316f81d88513c04f2028d434508
Liberbell <liberbell@gmail.com>
2021-06-02 08:54:54 +0900

    main4

--- 11/usemap/main.go
+++ 11/usemap/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
 	in := bufio.NewScanner(os.Stdin)
 	in.Split(bufio.ScanWords)
+
+	for in.Scan() {
+		fmt.Println(in.Text())
+	}
 }
