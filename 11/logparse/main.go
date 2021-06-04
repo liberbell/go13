0c1e010b42e1456e71d1b43f6f017c75d7b4f1fa
Liberbell <liberbell@gmail.com>
2021-06-04 09:12:59 +0900

    main5

--- 11/logparse/main.go
+++ 11/logparse/main.go
@@ -12,6 +12,9 @@ func main() {
 
 	for in.Scan() {
 		fields := strings.Fields(in.Text())
-		fmt.Printf("domain: %s - visits: %s\n", fields[0], fields[1])
+
+		domain := fields[0]
+		visits := fields[1]
+		fmt.Printf("domain: %s - visits: %s\n", domain, visits)
 	}
 }
