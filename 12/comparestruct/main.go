f2f51a05ec7b8c3eac4ed688664f50c534a3d981
Liberbell <liberbell@gmail.com>
2021-06-07 08:51:01 +0900

    main5

--- 12/comparestruct/main.go
+++ 12/comparestruct/main.go
@@ -10,10 +10,14 @@ func main() {
 	song1 := song{title: "wonderwall", artist: "Oasis"}
 	song2 := song{title: "super sonic", artist: "Oasis"}
 
+	song1 = song2
+
 	fmt.Printf("Song1: %+v\n", song1)
 	fmt.Printf("Song2: %+v\n", song2)
 
 	if song1.title == song2.title && song1.artist == song2.artist {
-		fmt.Println("song are equal.")
+		fmt.Println("songs are equal.")
+	} else {
+		fmt.Println("songs are not equal.")
 	}
 }
