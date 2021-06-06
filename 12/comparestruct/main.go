fdd8ba2b01e2455af0d8d7a3c21082dfabce2fd0
Liberbell <liberbell@gmail.com>
2021-06-07 08:43:27 +0900

    main2

--- 12/comparestruct/main.go
+++ 12/comparestruct/main.go
@@ -6,5 +6,6 @@ func main() {
 	song1 := song{title: "wonderwall", artist: "Oasis"}
 	song2 := song{title: "super sonic", artist: "oasis"}
 
-	fmt.Printf("song1: %+v\n", song1)
+	fmt.Printf("Song1: %+v\n", song1)
+	fmt.Printf("Song2: %+v\n", song2)
 }
