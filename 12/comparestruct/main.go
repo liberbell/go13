4199d7f06560b6b061db20bf7baa9d3dcb4b3160
Liberbell <liberbell@gmail.com>
2021-06-07 08:42:37 +0900

    main1

--- 12/comparestruct/main.go
+++ 12/comparestruct/main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	song1 := song{title: "wonderwall", artist: "Oasis"}
+	song2 := song{title: "super sonic", artist: "oasis"}
+
+	fmt.Printf("song1: %+v\n", song1)
 }
