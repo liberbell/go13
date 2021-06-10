27c9df338cd081161920f11f6fc8f237c813b49f
Liberbell <liberbell@gmail.com>
2021-06-10 09:06:59 +0900

    main4

--- 12/decodejson/main.go
+++ 12/decodejson/main.go
@@ -1,14 +1,17 @@
 package main
 
+type permissions map[string]bool
+
 type user struct {
-	user        string
-	password    string
-	permissions map[string]bool
+	user     string
+	password string
+	permissions
 }
 
 func main() {
 	users := []user{
 		{"inanc", "1234", nil},
 		{"god", "42", map[string]bool{"admin": true}},
+		{"devil", "666", map[string]bool{"write": true}},
 	}
 }
