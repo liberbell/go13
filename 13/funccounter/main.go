544911b7b29532d5d9225a77f9f038d84d834be2
Liberbell <liberbell@gmail.com>
2021-06-13 16:18:42 +0900

    main3

--- 13/funccounter/main.go
+++ 13/funccounter/main.go
@@ -1,22 +1,9 @@
 package main
 
-import "fmt"
-
 var N int
 
 func main() {
 	incrN()
 	incrN()
 	showN()
-}
-
-func showN() {
-	if N == 0 {
-		return
-	}
-	fmt.Printf("showN       : N is %d\n", N)
-}
-
-func incrN() {
-	N++
 }
