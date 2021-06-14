25dd35ab04637509be94a41f4fd4fb811305a72a
Liberbell <liberbell@gmail.com>
2021-06-15 08:50:47 +0900

    parser2

--- 13/parsefunc/parser.go
+++ 13/parsefunc/parser.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func newParser() parser {
 	return parser{sum: make(map[string]result)}
+}
+
+func parse() {
+	fields := strings.Fields(in.Text())
+	if len(fields) != 2 {
+		fmt.Printf("wrong input: %v (line #%d)\n", fields, p.lines)
+		return
+	}
+
+	domain := fields[0]
+	visits, err := strconv.Atoi(fields[1])
+	if visits < 0 || err != nil {
+		fmt.Printf("wrong input: %q (lines #%d)\n", fields[1], p.lines)
+		return
+	}
 }
