3c532b1f0d38bee2442770c63872a038fc7c279a
Liberbell <liberbell@gmail.com>
2021-06-16 08:34:37 +0900

    parser3

--- 13/parsefunc/parser.go
+++ 13/parsefunc/parser.go
@@ -16,14 +16,14 @@ func parse(p parser, line string) {
 	fields := strings.Fields(line)
 	if len(fields) != 2 {
 		fmt.Printf("wrong input: %v (line #%d)\n", fields, p.lines)
-		return
+		return parsed
 	}
 
 	domain := fields[0]
 	visits, err := strconv.Atoi(fields[1])
 	if visits < 0 || err != nil {
 		fmt.Printf("wrong input: %q (lines #%d)\n", fields[1], p.lines)
-		return
+		return parsed
 	}
 	return result{domain: domain, visits: vists}
 }
