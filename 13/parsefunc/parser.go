98200dd439a5bf3678c6f9274afd0e0a6606b26d
Liberbell <liberbell@gmail.com>
2021-06-16 08:47:47 +0900

    parser7

--- 13/parsefunc/parser.go
+++ 13/parsefunc/parser.go
@@ -6,6 +6,18 @@ import (
 	"strings"
 )
 
+type result struct {
+	domain string
+	visits int
+}
+
+type parser struct {
+	sum     map[string]result
+	domains []string
+	total   int
+	lines   int
+}
+
 func newParser() parser {
 	return parser{sum: make(map[string]result)}
 }
