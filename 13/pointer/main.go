c23ad1cff065616c524c03ec801a036b541422e4
Liberbell <liberbell@gmail.com>
2021-06-18 09:44:13 +0900

    main8

--- 13/pointer/main.go
+++ 13/pointer/main.go
@@ -5,12 +5,14 @@ import "fmt"
 func main() {
 	var (
 		counter int
+		V       int
 		P       *int
 	)
 
 	counter = 100
 
 	P = &counter
+	V = *P
 
 	// if P == nil {
 	// 	fmt.Printf("P is nil and its address is %p\n", P)
@@ -22,4 +24,5 @@ func main() {
 
 	fmt.Printf("counter: %-13d addr: %13p\n", counter, &counter)
 	fmt.Printf("P      : %-13p addr: %13p *P: %-13d\n", P, &P, *P)
+	fmt.Printf("V      : %-13p addr: %13p *P: %-13d\n", P, &P, *P)
 }
