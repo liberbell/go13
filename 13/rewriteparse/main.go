f7da0b76b40bcffeb1ca0cfe449be08e4fd3bb47
Liberbell <liberbell@gmail.com>
2021-06-21 09:13:46 +0900

    main2

--- 13/rewriteparse/main.go
+++ 13/rewriteparse/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	p := newParser()
+
+	in := bufio.NewScanner(os.Stdin)
+	for in.Scan() {
+		p.lines++
+
+		parsed, err := parse(p, in.Text())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("main.parser before: \n\t%+v\n", p)
+		p = update(p, parsed)
+		fmt.Printf("main.parser after: \n\t%+v\n", p)
+
+	}
+	sort.Strings(p.domains)
+
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
+	fmt.Println(strings.Repeat("-", 45))
+
+	for _, domain := range p.domains {
+		parsed := p.sum[domain]
+		fmt.Printf("%-30s %10d\n", domain, parsed.visits)
+	}
+
+	fmt.Printf("\n%-30s %10d\n", "TOTAL", p.total)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("> Err:", err)
+	}
+}
