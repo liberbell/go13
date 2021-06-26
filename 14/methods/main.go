fb1c8441d787d418d22405227e03a3f2ab584a8d
Liberbell <liberbell@gmail.com>
2021-06-26 09:56:23 +0900

    main2

--- 14/methods/main.go
+++ 14/methods/main.go
@@ -1,20 +1,11 @@
 package main
 
 func main() {
-	modydick := book{
-		title: "mody dick",
-		price: 10,
-	}
-
-	minecraft := game{
-		title: "minecraft",
-		price: 20,
-	}
-
-	tetris := game{
-		title: "tetris",
-		price: 5,
-	}
+	var (
+		modydick  = book{title: "mody dick", price: 10}
+		minecraft = game{title: "minecraft", price: 20}
+		tetris    = game{title: "tetris", price: 5}
+	)
 
 	modydick.print()
 	minecraft.print()
