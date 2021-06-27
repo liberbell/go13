c051fd0c3b1e75b1a4913139fad1253f7c95b5a7
Liberbell <liberbell@gmail.com>
2021-06-27 10:48:16 +0900

    main1

--- 14/methods/main.go
+++ 14/methods/main.go
@@ -1,22 +1,22 @@
 package main
 
 func main() {
-	// 	var (
-	// 		modydick  = book{title: "mody dick", price: 10}
-	// 		minecraft = game{title: "minecraft", price: 20}
-	// 		tetris    = game{title: "tetris", price: 5}
-	// 	)
+		var (
+			modydick  = book{title: "mody dick", price: 10}
+			minecraft = game{title: "minecraft", price: 20}
+			tetris    = game{title: "tetris", price: 5}
+		)
 
-	// 	minecraft.discount(.5)
+		minecraft.discount(.5)
 
-	// 	modydick.print()
-	// 	minecraft.print()
-	// 	tetris.print()
-	// }
-
-	var h huge
-
-	for i := 0; i < 10; i++ {
-		h.addr()
+		modydick.print()
+		minecraft.print()
+		tetris.print()
 	}
+
+	// var h huge
+
+	// for i := 0; i < 10; i++ {
+	// 	h.addr()
+	// }
 }
