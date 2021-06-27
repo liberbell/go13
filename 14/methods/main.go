0d72a25e50e5d65e4de9d306921f241df95cc22a
Liberbell <liberbell@gmail.com>
2021-06-27 10:50:48 +0900

    main3

--- 14/methods/main.go
+++ 14/methods/main.go
@@ -2,13 +2,13 @@ package main
 
 func main() {
 	var (
-		modydick  = book{title: "mody dick", price: 10}
+		// modydick  = book{title: "mody dick", price: 10}
 		minecraft = game{title: "minecraft", price: 20}
 		tetris    = game{title: "tetris", price: 5}
 	)
 
 	var items []*game
-	items = append(items, &minecraft, &tetris, &modydick)
+	items = append(items, &minecraft, &tetris)
 
 	minecraft.discount(.5)
 
