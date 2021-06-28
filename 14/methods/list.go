ec5677560f499692f0edbed421a2a44554ccfcdd
Liberbell <liberbell@gmail.com>
2021-06-28 16:27:58 +0900

    list1

--- 14/methods/list.go
+++ 14/methods/list.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
+
 type list []*game
 
 func (l list) print() {
+	if len(l) == 0 {
+		fmt.Println("Sorry. We`re waiting for delivery.")
+	}
 	for _, it := range l {
 		it.print()
 	}
