22bf482f942df489aa847d4bc0e22f1a4e38ffbb
Liberbell <liberbell@gmail.com>
2021-06-30 09:11:20 +0900

    toy2

--- 14/methods/toy.go
+++ 14/methods/toy.go
@@ -2,31 +2,15 @@ package main
 
 import "fmt"
 
-type printer interface {
-	print()
+type game struct {
+	title string
+	price money
 }
 
-type list []printer
-
-func (l list) print() {
-	if len(l) == 0 {
-		fmt.Println("Sorry. We`re waiting for delivery.")
-		return
-	}
-
-	for _, it := range l {
-		// fmt.Printf("(%-10T) --> ", it)
-		it.print()
-	}
+func (g *game) print() {
+	fmt.Printf("%-15s: %s\n", g.title, g.price.string())
 }
 
-func (l list) discount(ratio float64) {
-	for _, it := range l {
-		g, isGame := it.(*game)
-		if !isGame {
-			continue
-		}
-		g.discount(ratio)
-	}
-
+func (g *game) discount(ratio float64) {
+	g.price *= money(1 - ratio)
 }
