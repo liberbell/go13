f9f0019941943d71f30a4a195ec394112afb2638
Liberbell <liberbell@gmail.com>
2021-07-02 15:44:10 +0900

    main4

--- 14/methods/main.go
+++ 14/methods/main.go
@@ -4,7 +4,7 @@ func main() {
 	store := list{
 		book{title: "mody dick", price: 10, published: 118281600},
 		book{title: "odyssey", price: 10, published: "723622855"},
-		book{title: "hobbit", price: 10},
+		book{title: "hobbit", price: 25},
 		&game{title: "minecraft", price: 20},
 		&game{title: "tetris", price: 5},
 		puzzle{title: "rubik`s cube", price: 5},
