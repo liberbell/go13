07a78956194e942bbbbe1cdf2c80f7dff3d6b2fa
Liberbell <liberbell@gmail.com>
2021-07-03 11:58:45 +0900

    main5

--- 14/methods/main.go
+++ 14/methods/main.go
@@ -4,7 +4,7 @@ func main() {
 	store := list{
 		&book{product{"mody dick", 10}, 118281600},
 		&book{product{"odyssey", 10}, "723622855"},
-		&book{product{"hobbit", 25}},
+		&book{product{"hobbit", 25}, nil},
 		&game{product{"minecraft", 20}},
 		&game{product{"tetris", 5}},
 		&puzzle{product{"rubik`s cube", 5}},
