be18324140192a3ca0bd1eb3aeb27549b2148f46
Liberbell <liberbell@gmail.com>
2021-07-05 08:56:39 +0900

    main1

--- 15/notallinterface/main.go
+++ 15/notallinterface/main.go
@@ -5,10 +5,10 @@ func main() {
 		{title: "mody dick", price: 10, released: 118281600},
 		{title: "odyssey", price: 10, released: "723622855"},
 		{title: "hobbit", price: 25},
-		{title: "minecraft", price: 20},
-		{title: "tetris", price: 5},
-		{title: "rubik`s cube", price: 5},
-		{title: "yoda", price: 150},
+		// {title: "minecraft", price: 20},
+		// {title: "tetris", price: 5},
+		// {title: "rubik`s cube", price: 5},
+		// {title: "yoda", price: 150},
 	}
 
 	l.discount(.5)
