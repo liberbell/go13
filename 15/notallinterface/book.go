f0c51cd95d3c54705b534e533fd2319a1a184eed
Liberbell <liberbell@gmail.com>
2021-07-04 16:48:38 +0900

    book1

---
+++ 15/notallinterface/book.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type book struct {
+	product
+	published interface{}
+}
+
+func (b *book) print() {
+	b.product.print()
+
+	p := format(b.published)
+	fmt.Printf("\t - (%v)\n", p)
+}
+
+func format(v interface{}) string {
+	var t int
+
+	switch v := v.(type) {
+	case int:
+		t = v
+	case string:
+		t, _ = strconv.Atoi(v)
+	default:
+		return "unknown"
+	}
+
+	const layout = "2006/01/02"
+
+	u := time.Unix(int64(t), 0)
+	return u.Format(layout)
+}
