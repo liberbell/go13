62476ee3e89e92c9e75becf772ac7a5b9e798169
Liberbell <liberbell@gmail.com>
2021-07-04 16:49:59 +0900

    product2

--- 15/notallinterface/product.go
+++ 15/notallinterface/product.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type product struct {
 	title string
@@ -13,4 +17,22 @@ func (p *product) print() {
 
 func (p *product) discount(ratio float64) {
 	p.price *= money(1 - ratio)
+}
+
+func format(v interface{}) string {
+	var t int
+
+	switch v := v.(type) {
+	case int:
+		t = v
+	case string:
+		t, _ = strconv.Atoi(v)
+	default:
+		return "unknown"
+	}
+
+	const layout = "2006/01/02"
+
+	u := time.Unix(int64(t), 0)
+	return u.Format(layout)
 }
