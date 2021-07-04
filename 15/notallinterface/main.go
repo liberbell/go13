4056c71ed402fe0e9d7b657c9c8f9f27ca812ef1
Liberbell <liberbell@gmail.com>
2021-07-04 16:55:02 +0900

    main2

--- 15/notallinterface/main.go
+++ 15/notallinterface/main.go
@@ -2,13 +2,13 @@ package main
 
 func main() {
 	store := list{
-		&book{product{"mody dick", 10}, 118281600},
-		&book{product{"odyssey", 10}, "723622855"},
-		&book{product{"hobbit", 25}, nil},
-		&game{product{"minecraft", 20}},
-		&game{product{"tetris", 5}},
-		&puzzle{product{"rubik`s cube", 5}},
-		&toy{product{"yoda", 150}},
+		{product{"mody dick", 10}, 118281600},
+		{product{"odyssey", 10}, "723622855"},
+		{product{"hobbit", 25}, nil},
+		{product{"minecraft", 20}},
+		{product{"tetris", 5}},
+		{product{"rubik`s cube", 5}},
+		{product{"yoda", 150}},
 	}
 
 	store.discount(.5)
