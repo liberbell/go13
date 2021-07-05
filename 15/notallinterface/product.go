5f63e229a702c14f1758a708a6c2925d83bf7946
Liberbell <liberbell@gmail.com>
2021-07-05 09:00:42 +0900

    product2

--- 15/notallinterface/product.go
+++ 15/notallinterface/product.go
@@ -13,7 +13,7 @@ type product struct {
 }
 
 func (p *product) print() {
-	fmt.Printf("%s: %s (%s)\n", p.title, p.price.string(), format(p.released))
+	fmt.Printf("%s: %s (%s)\n", p.title, p.price.string(), p.released.string())
 }
 
 func (p *product) discount(ratio float64) {
