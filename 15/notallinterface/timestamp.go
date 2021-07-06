d968604117a441ee5205cfd4ded381750f2444c4
Liberbell <liberbell@gmail.com>
2021-07-06 15:45:04 +0900

    timestamp1

--- 15/notallinterface/timestamp.go
+++ 15/notallinterface/timestamp.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type timestamp struct {
 	time.Time
@@ -13,4 +16,18 @@ func (ts timestamp) string() string {
 
 	const layout = "2006/01"
 	return ts.Format(layout)
+}
+
+func toTimestamp(v interface{}) (ts timestamp) {
+	var t int
+
+	switch v := v.(type) {
+	case int:
+		t = v
+	case string:
+		t, _ = strconv.Atoi(v)
+	}
+
+	ts.Time = time.Unix(int64(t), 0)
+	return ts
 }
