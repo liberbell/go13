76cb70eaca878384b141ca4dec3f8bf1e60e8e94
Liberbell <liberbell@gmail.com>
2021-07-06 22:08:32 +0900

    main3

--- 15/notallinterface/main.go
+++ 15/notallinterface/main.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	l := list{
-		{title: "mody dick", price: 10, released: toTimestamp(118281600)},
-		{title: "odyssey", price: 10, released: toTimestamp("723622855")},
-		{title: "hobbit", price: 25},
-		// {title: "minecraft", price: 20},
-		// {title: "tetris", price: 5},
-		// {title: "rubik`s cube", price: 5},
-		// {title: "yoda", price: 150},
-	}
+	// l := list{
+	// 	{title: "mody dick", price: 10, released: toTimestamp(118281600)},
+	// 	{title: "odyssey", price: 10, released: toTimestamp("723622855")},
+	// 	{title: "hobbit", price: 25},
+	// 	// {title: "minecraft", price: 20},
+	// 	// {title: "tetris", price: 5},
+	// 	// {title: "rubik`s cube", price: 5},
+	// 	// {title: "yoda", price: 150},
+	// }
 
-	l.discount(.5)
-	// l.print()
-	fmt.Print(l)
+	// l.discount(.5)
+	// // l.print()
+	// fmt.Print(l)
+
+	var pocket money = 10
+	fmt.Println("I have", pocket)
 
 }
