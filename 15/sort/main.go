7b747b8bc00a069ce39e191d46729717e9439d6e
Liberbell <liberbell@gmail.com>
2021-07-06 22:12:14 +0900

    main2

--- 15/sort/main.go
+++ 15/sort/main.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
 	l := list{
 		{title: "mody dick", price: 10, released: toTimestamp(118281600)},
 		{title: "odyssey", price: 10, released: toTimestamp("723622855")},
 		{title: "hobbit", price: 25},
 	}
+
+	sort.Sort()
+	fmt.Print(l)
 }
